eb4da67f546b03608d9593fe9abd5bba6ead2248
jelson94 <jelson.eng.civil@gmail.com>
2024-02-24 11:23:15 -0300

    Calculo área do círculo

--- areaGeom.py
+++ areaGeom.py
@@ -1,7 +1,13 @@
 #testandoooooo 
+from math import pi
 
 def areaRet(ladoA, ladoB):
 
     area = ladoA * ladoB
 
+    return area
+
+def areaCir(diâmetro):
+
+    area = pi * (diâmetro ** 2) / 4
     return area
